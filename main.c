8e42976a0a1a54d522ae6f8d32e25d5ef6ba3427
Elia Sforna <esforna0110@gmail.com>
2015-02-20 09:58:38 +0100

    main.c

--- main.c
+++ main.c
@@ -1,8 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+const int N = 5;
 int main(int argc, char** argv) {
-
+    int a,b,c,d,e;
+    int x;
+    int ymin;
+    int zmax;
+    int ris1;
+    int ris2;
+    int NP;
+   
+    printf("Dovrai inserire l'età di 5 persone:\n\n");
+    printf("Inserisci l'età della 1° persona\n");
+    scanf("%d" , &a);
+    printf("Inserisci l'età della 2° persona\n");
+    scanf("%d" , &b);
+    printf("Inserisci l'età della 3° persona\n");
+    scanf("%d" , &c);
+    printf("Inserisci l'età della 4° persona\n");
+    scanf("%d" , &d);
+    printf("Inserisci l'età della 5° persona\n");
+    scanf("%d" , &e);
+   
+    for(ymin=0; x<18; ymin++) {
+        ris1 = ymin;
+    
+    printf("I minorenni sono: %d" , ris1);
+    return 0;
+    }
+    for(zmax=0; x<18; zmax++) {
+    ris2= zmax;
+    printf("I maggiorenni sono: %d" , ris2);
+        return 0;
+    }
     return (EXIT_SUCCESS);
+    
+    
+    
+    
+    
+    
+        
+    
+ 
+ 
+   
 }
+
+                
+              
+    
+    
+   
 
